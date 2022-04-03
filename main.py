a8c15d7f1514452653bc96ddaccda38e3d952a36
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-03 04:35:09 -0600

    Adding few comments

--- main.py
+++ main.py
@@ -60,6 +60,14 @@
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 
 
+
+'''
+Start Date:
+4/3/2022
+Current Time:
+4:34 am
+'''
+
 import random
 import os
 
