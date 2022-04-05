c8d12c2667b349402358a553f99b4a74e205a72a
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-04 18:54:32 -0600

    While loop was not operating correctly

--- main.py
+++ main.py
@@ -5,52 +5,70 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 from art import logo
 
-def start(menu, playing):
-  play_again = str(input("Do you want to play a game of Blackjack? Type 'y' or 'n': "))
-  if (play_again).lower() == 'y':
+def start(playing):
+  play = str(input("Do you want to play a game of Blackjack? Type 'y' or 'n': "))
+  if play == 'y':
     os.system('cls||clear')
     print(logo)
-    playing = True
     return playing
-  elif play_again.lower() == 'n':
-    menu = False
-    return menu
+  elif play == "n":
+    playing = False
+    return playing
+  else:
+    playing = False
+    return playing
 
 def player_cards(deck, score):
-  for x in user_deck:
+  for x in deck:
     score += x
   print (f"Your cards are: {deck}, current score: {score}")
 
-def hit(deck, score):
+def hit(deck, score, busted):
   deck.append(cards[random.randint(2, 11)])
-    
-def bust(deck, score):
-   print (f"You have bust with a score of {score}. Your cards were: {deck}")
-  
+  for x in deck:
+    score += x
 
-while True:
-    play = input("Do you want to play Blackjack? Type 'y' or 'n' ")
-    if play == 'y':
-        score = 0
-        user_deck = []
-        user_deck.append(cards[random.randint(2,11)])
-        user_deck.append(cards[random.randint(2,11)])
-        player_cards(user_deck, score)
-
-        while score <= 21:
-          for x in user_deck:
-            score += x
-          print(f'THE SCOREEEEE: {score}')
-          user_hit = input('Would you like to hit? ')
-          if user_hit == 'y':
-            hit(user_deck, score)
-        
-        bust(user_deck, score)
-          
+  if score > 21:
+    print(f'Darn! You have busted. Your final deck was {deck} with a score of {score}.')
+    busted = True
+    return busted
+  else:
+    print(f'Your new cards are: {deck}, current score: {score}')
+    return busted
 
 
+playing = True
+while playing:
+    playing = start(playing)
+    if playing == True:
+      user_deck = []
+      score = 0
+      busted = False
+      user_deck.append(cards[random.randint(2,11)])
+      user_deck.append(cards[random.randint(2,11)])
+      player_cards(user_deck, score)
+      while busted != True:
+        user_hit = input('Would you like to hit? ')
+        if user_hit == 'y':
+          busted = hit(user_deck, score, busted)
+    else:
+      break
+print('Thank you for playing!')
 
 
+#     play = input("Do you want to play Blackjack? Type 'y' or 'n' ")
+#     if play == 'y':
+#         user_deck = []
+#         score = 0
+#         user_deck.append(cards[random.randint(2,11)])
+#         user_deck.append(cards[random.randint(2,11)])
+#         player_cards(user_deck, score)
+
+    #       user_hit = input('Would you like to hit? ')
+    #       if user_hit == 'y':
+    #         hit(user_deck, score)
+    # else:
+    #   playing = False
 
 # menu = True
 # playing = True
